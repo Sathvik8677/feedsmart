ff5a17e1aa68b0860ff28aa0237140d2c1d89386
Bheemarapu Sathvik <sathvik8677@gmail.com>
2026-03-29 12:31:54 +0530

    final fix init_db placement

--- app.py
+++ app.py
@@ -929,6 +929,7 @@ def verify_otp():
         return jsonify({'success': False})
 
 
-if __name__ == '__main__':
-    init_db()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+app = Flask(__name__)
+app.secret_key = 'feedsmart_v3_ultra_2026'
+
+init_db()
